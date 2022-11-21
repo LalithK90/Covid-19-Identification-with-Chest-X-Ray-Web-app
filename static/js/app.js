f0602576ba0657fa6a8dcc89572b1d00d796f900
lalithk90 <asakahatapitiya@gmail.com>
2022-11-21 10:32:25 +0530

    after changes

---
+++ static/js/app.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function () {
+    result_hide();
+});
+
+function result_hide () {
+    $("#result-show").hide();
+} function result_show () {
+    $("#result-show").show();
+}
+let base64Image;
+$("#image-selector").change(function () {
+    result_hide();
+    let reader = new FileReader();
+    reader.onload = function (e) {
+        let dataURL = reader.result;
+        $('#selected-image').attr("src", dataURL);
+        base64Image = dataURL.replace(/^data:image\/(png|jpg|jpeg);base64,/, "");
+        //console.log(base64Image);
+    };
+    reader.readAsDataURL($("#image-selector")[0].files[0]);
+    $("#result").text("");
+    $("#probability").text("");
+
+});
+
+$("#predict-button").click(function () {
+    let message = {
+        image: base64Image
+    };
+    console.log(message);
+
+    if (message == null) {
+        console.log("sdsad");
+        window.alert("There is no selected image");
+    } else {
+        result_show();
+        $.post("http://127.0.0.1:5000/predict", JSON.stringify(message),
+            function (response) {
+                $("#result").text(response.prediction.result);
+                $("#probability").text(response.prediction.accuracy.toFixed(2));
+                console.log(response);
+            });
+    }
+
+});
